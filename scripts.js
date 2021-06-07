a8e339c8cdf246f6d77ca40c83e5b91ce07c1100
JJohnyy <84328224+JJohnyy@users.noreply.github.com>
2021-06-07 06:14:50 +0200

    ex2

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let color = "purple";
+let number = "10";
+let word = "cool";
+
+if (color == "purple") {
+$("north").css 
+("background-color", "purple");
+};
+
+$("#south").text("text");
+
+
+
+if (number > 100) {
+  console.log("whoah, that is a big number");
+} else if (number <=100) {
+  console.log("just a regular number, please");
+};
+
+if (word == "cool") {
+  $("south").css 
+  ("text", "Power of DOM");
+    else {
+      $("west").css 
+  ("text", "Power of DOM");
+    }
+  };
